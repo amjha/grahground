3082d325644adfa9834acc46a6deea31d1f089e9
amjha <amitjha@usc.edu>
2019-10-07 23:26:40 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header, Button, Icon } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.buttonList = [
+      {id: 1, name: 'd3 Map', icon:'map'},
+      {id: 2, name: 'Pie Chart', icon:'chart pie'},
+      {id: 3, name: 'Population', icon:'users'},
+      {id: 4, name: 'Bar Chart', icon:'chart bar'},
+      {id: 5, name: '3D Bar', icon:'hdd'},
+      {id: 6, name: 'Chart Line', icon:'chart line'},
+      {id: 7, name: 'Knn Map', icon:'pied piper'},
+      {id: 8, name: 'Color picker', icon:'lab'},
+      {id: 9, name: 'Shape shifter', icon:'asl'}
+    ]
+  }
+  handleClick = (id=9) => {
+    console.log("Clicked "+id)
+    alert(id)
+  }
+  componentDidMount(){
+    
+  }
+  render() {
+    return (
+      <div className="App">
+        <span className="header"><h2>Graphground</h2></span>
+        <section id="main_buttons">
+          <div id="graphs_button" className="graphs_button">
+            {this.buttonList && this.buttonList.map((button, i)=>
+              <div key={i}>
+              <Button animated='vertical' onClick={this.handleClick.bind(this, button.id)} secondary>
+              <Button.Content visible>{button.name}</Button.Content>
+              <Button.Content hidden>
+                <Icon name={button.icon} />
+              </Button.Content>
+            </Button>
+            </div>
+            ) }
+            
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
